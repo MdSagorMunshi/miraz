5481ef95863aadd6cc512dc41f53bc0f894721ee
Lee Siwoo <leesiwoo@layek.leesiwoo.me>
2023-09-18 21:26:44 +0600

    intial commit

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,94 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Missionary</li>
+        <li>Doggy Style</li>
+        <li>Legs in the air</li>
+        <li>Cowgirl</li>
+        <li>Oral</li>
+        <li>Lotus</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>can't say</li>
+        <li>can't say</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>NGSC (NAIONAL GAY SCHOOL CERTIFICATE)</li>
+        <li>NGCC (NATIONAL GAY COLLAGE CERTIFICATE)</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about-image.png" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            IHi, I'm Miraz! I'm an 18-year-old gay person, and I believe in living life authentically and embracing who I am. My journey as a gay individual has been filled with self-discovery, self-acceptance, and the support of amazing friends and allies. I'm passionate about promoting diversity, inclusion, and equal rights for all. In my free time, I enjoy connecting with the LGBTQ+ community, exploring new hobbies, and making meaningful connections with people who share similar experiences. My hope is to continue spreading love, understanding, and positivity in the world, as we work toward a more inclusive and accepting society
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
