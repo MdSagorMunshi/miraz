7d5aab99c7321028b6adc2ef1472f3630be2ff5a
Md Sagor Munshi <121169140+MdSagorMunshi@users.noreply.github.com>
2023-12-28 06:03:07 -0600

    Update Footer.jsx

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="container p-12 flex justify-between">
         <span>MIRAZ</span>
         <p className="text-slate-600">All rights reserved.</p>
-        <a href="https://github.com/MdSagorMunshi" className="text-slate-600">Made By Layek Ahmed</a>
+        <a href="https://github.com/MdSagorMunshi" className="text-slate-600">Made By Ryan Shelby</a>
       </div>
     </footer>
   );
