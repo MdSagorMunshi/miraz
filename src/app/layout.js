5b0fb41aa4d275fc246e193bef3940d655847f69
Md Sagor Munshi <121169140+MdSagorMunshi@users.noreply.github.com>
2023-12-28 06:13:20 -0600

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,24 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import { Inter } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/react';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'CALL BOY MIRAZ',
   description: 'Hey..! I am Miraz',
-}
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <title>{metadata.title}</title>
+      </head>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
-  )
+  );
 }
